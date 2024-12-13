5136945bc6d12eebb4e7f3b464cb8ed9dcff619c
PressToCode <code.olfatfaiz@gmail.com>
2024-12-14 00:10:07 +0700

    Minor Visual Change to Event Template

--- resources/views/event/show.blade.php
+++ resources/views/event/show.blade.php
@@ -2,14 +2,16 @@
     {{-- Hero Section --}}
     <section class="tw-bg-[#4338CA] tw-text-white tw-p-8 tw-rounded-lg">
         <div class="tw-max-w-6xl tw-mx-auto">
-            <div class="tw-grid tw-grid-cols-1 md:tw-grid-cols-2 tw-gap-8">
-                <div>
-                    <h1 class="tw-text-4xl tw-font-bold tw-mb-4">{{ $event->title }}</h1>
-                    <div class="tw-bg-white tw-w-full tw-h-48 tw-rounded-lg">
+            <div class="tw-grid tw-grid-cols-1 md:tw-grid-cols-2 tw-gap-8 tw-pt-10">
+                <div class="">
+                    <h1 class="tw-text-4xl tw-font-bold tw-mb-4 tw-col-span-1">{{ $event->title }}</h1>
+                    <div class="tw-bg-white tw-w-full tw-rounded-lg">
                         <img src="{{ URL::asset('images/cardPlaceholder.svg') }}" alt="{{ $event->title }}" class="tw-w-full tw-h-full tw-object-cover tw-rounded-lg">
                     </div>
                 </div>
                 <div>
+                    <!-- I'm pretty sure there's a better way around this bruh -->
+                    <h1 class="tw-text-4xl tw-font-bold tw-mb-4 tw-hidden md:tw-block md:tw-text-transparent">{{ $event->title }}</h1>
                     <p class="tw-mb-6">{{ $event->description }}</p>
                     <div class="tw-flex tw-gap-4 tw-mb-8">
                         <a href="#about" class="tw-bg-white/20 tw-px-6 tw-py-2 tw-rounded-full">Selengkapnya</a>
@@ -39,9 +41,9 @@
 
     {{-- About Section --}}
     <section id="about" class="tw-py-16">
-        <div class="tw-max-w-6xl tw-mx-auto">
+        <div class="tw-w-full tw-px-8">
             <h2 class="tw-text-3xl tw-font-bold tw-mb-6">ABOUT</h2>
-            <p class="tw-max-w-2xl">{{ $event->full_description }}</p>
+            <p class="tw-max-w-2xl">{{ $event->description }}</p>
         </div>
     </section>
 </x-app-layout>
