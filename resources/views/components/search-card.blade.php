a7f6b1f967900ba372ac1a7cde05eaa1b4b04ce4
PressToCode <code.olfatfaiz@gmail.com>
2024-12-13 14:44:54 +0700

    Refined Search Bar Control & Page

--- resources/views/components/search-card.blade.php
+++ resources/views/components/search-card.blade.php
@@ -1,3 +1,12 @@
-<div>
-    TEST
-</div>
+@props(['title', 'description', 'date', 'image'])
+
+<a href="{{ Route('dashboard') }}" class="tw-grid tw-grid-cols-10 overflow-hidden tw-rounded-md tw-mb-5 tw-border tw-transition tw-ease-out hover:tw-scale-105 tw-duration-200">
+    <div class="tw-col-span-3">
+    <img src="{{ $image ?? URL::asset('images/cardPlaceholder.svg') }}" class="tw-h-full tw-w-full tw-object-cover" alt="Event image">
+    </div>
+    <div class="tw-flex tw-flex-col tw-col-span-7 tw-py-2 tw-px-5">
+        <h3 class="tw-flex tw-flex-initial tw-text-xl">{{ $title }}</h3>
+        <p class="tw-flex tw-flex-auto tw-text-justify">{{ $description }}</p>
+        <p class="tw-flex tw-flex-initial">{{ $date }}</p>
+    </div>
+</a>
--- resources/views/search/search.blade.php
+++ resources/views/search/search.blade.php
@@ -1,17 +1,21 @@
 <x-app-layout>
     @section('title', 'Search Events - Filkom Pulse')
     <div class="pt-5">
-        <div class="pt-5"></div>
-        <!-- data, total, per_page, current_page -->
-        @if(isset($result))
-            @foreach ($result->items() as $event)
-                <h3>{{ $event['title'] }}</h3>
-            @endforeach
-            <div>
-                {{ $result->links() }}
-            </div>
-        @else
-            <p>No search query provided.</p>
-        @endif
+        <div class="pt-5 px-5">
+            <!-- data, total, per_page, current_page -->
+            @if(!empty($result->items()))
+                @foreach ($result->items() as $event)
+                    <x-search-card
+                        :title="$event['title']"
+                        :description="$event['description']"
+                        :date="$event['date']" />
+                @endforeach
+                <div>
+                    {{ $result->links() }}
+                </div>
+            @else
+                <p class="tw-text-5xl tw-text-center tw-font-bold tw-animate-bounce">Sorry, No Event Match Your Search <i class="bi bi-emoji-frown"></i></p>
+            @endif
+        </div>
     </div>
 </x-app-layout>
