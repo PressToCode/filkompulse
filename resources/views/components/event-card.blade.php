1d3ffc9a4d8dd35d1e1312e27333ee6bae5497a5
PressToCode <code.olfatfaiz@gmail.com>
2024-12-18 14:18:12 +0700

    Minor Card Fix

--- resources/views/components/event-card.blade.php
+++ resources/views/components/event-card.blade.php
@@ -1,4 +1,4 @@
-<div class="tw-bg-[#322F35]/50 text-white rounded p-4 tw-h-full tw-flex tw-flex-col">
+<div class="tw-bg-[#322F35]/50 text-white rounded p-4 tw-h-full tw-flex tw-flex-col tw-justify-between">
     <div class="d-flex align-items-center gap-3 mb-3">
         <div class="tw-bg-[#6c5dd3] rounded-circle d-flex align-items-center justify-content-center" style="width: 40px; height: 40px;">
             <span class="fw-bold">{{ $avatarLetter }}</span>
@@ -11,7 +11,7 @@
     <img src="{{ $imageUrl ?? URL::asset('images/cardPlaceholder.svg') }}" class="img-fluid tw-w-full mb-3 rounded tw-max-h-40 tw-object-cover tw-overflow-hidden" alt="Event image">
     <h4 class="fw-bold mb-2">{{ $title }}</h4>
     <p class="tw-text-slate-300/80 small mb-3">{{ $dateTime }} | {{ $location }}</p>
-    <p class="text-light small mb-4">{{ $description }} </p>
+    <p class="text-light small mb-4 tw-text-justify">{{ $description }} </p>
     <a href="{{ route('event.show', $eventId) }}" class="tw-self-center">
         <button class="btn tw-bg-[#6c5dd3] hover:tw-bg-[#6c5dd3]/80 px-4 rounded-pill">Click Here!</button>
     </a>
